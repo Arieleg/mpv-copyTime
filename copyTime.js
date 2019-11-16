7332bf4236cd3e549d7913a0324220d64d1dae1f
Arieleg <arielgamboa@outlook.com>
2019-11-16 12:51:07 -0300

    first commit

---
+++ copyTime.js
@@ -0,0 +1,27 @@
+function setClipboard(text) {
+    mp.commandv("run", "powershell", "set-clipboard", text);
+}
+
+function formatTime(time) {
+    var r = "" + time;
+    while (r.length < 2) {
+        r = "0" + r;
+    }
+    return r;
+}
+
+function copyTime() {
+    var time_pos = mp.get_property_number("time-pos");
+    var time_seg = time_pos % 60;
+    time_pos -= time_seg;
+    var time_hours = Math.floor(time_pos / 3600);
+    time_pos -= time_hours * 3600;
+    var time_minutes = time_pos / 60;
+    var time_ms = time_seg - Math.floor(time_seg);
+    time_seg -= time_ms
+    var time = formatTime(time_hours) + ":" + formatTime(time_minutes) + ":" + formatTime(time_seg) + "." + time_ms.toFixed(9).toString().split(".")[1];
+    setClipboard(time)
+    mp.osd_message(text, 3);    
+}
+
+mp.add_key_binding("Ctrl+Alt+c", "copyTime", copyTime);
