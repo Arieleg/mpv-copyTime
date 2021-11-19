df98f68b6ec5d4a0993468278f1a586e7a7aa44b
Matthew Broadway <mattdbway@gmail.com>
2021-11-19 21:00:25 +0000

    improved MacOS support

--- README.md
+++ README.md
@@ -8,15 +8,12 @@ By default, the script is bound to "Ctrl + c"
 # Installation
 
 ## Windows
-For Windows use `copyTime.js` or `copyTime.lua`, the one you prefer.
-
-* Put the selected script in your scripts folder (usually `"%APPDATA%\mpv\scripts"`).
+* Put the copyTime.lua script in your scripts folder (usually `"%APPDATA%\mpv\scripts"`).
 
 The script needs PowerShell to work.
 
 ## GNU/Linux + MacOS
 
-For use `copyTime.lua`, which supports Windows, GNU/Linux and MacOS.
 `Powershell` is used on Windows, `xclip` is used on GNU/Linux and `pbcopy` is used on MacOS to access the system clipboard.
 
 * Put `copyTime.lua` in your scripts folder (usually `"~/.config/mpv/scripts/"`).
--- copyTime.lua
+++ copyTime.lua
@@ -2,41 +2,51 @@ require 'mp'
 require 'mp.msg'
 
 -- Copy the current time of the video to clipboard.
--- https://github.com/Arieleg/mpv-copyTime
 
 
-local function get_platform()
+local function platform_type()
     -- based on https://stackoverflow.com/a/30960054
+    -- cannot distinguish between linux and MacOS reliably
     local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
     if BinaryFormat == "dll" then
-        return "Windows"
-    elseif BinaryFormat == "so" then
-        return "Linux"
-    elseif BinaryFormat == "dylib" then
-        return "MacOS"
+        return "windows"
     else
-        return "Unknown"
+        return "unix"
     end
 end
 
+local function command_exists(cmd)
+    local pipe = io.popen("type " .. cmd .. " > /dev/null 2> /dev/null; echo -n $?", "r")
+    return pipe:read() == "0"
+end
+
+local function divmod(a, b)
+    return a / b, a % b
+end
 
 
 local function set_clipboard(text)
-    local platform = get_platform()
+    local platform = platform_type()
 
-    if platform == "Linux" then
-        local pipe = io.popen("xclip -silent -in -selection clipboard", "w")
-        pipe:write(text)
-        pipe:close()
-
-    elseif platform == "Windows" then
+    if platform == "windows" then
         mp.commandv("run", "powershell", "set-clipboard", text)
 
-    elseif platform == "MacOS" then
-        local pipe = io.popen("pbcopy", "w")
-        pipe:write(text)
-        pipe:close()
+    elseif platform == "unix" then
+        if command_exists("xclip") then
+            -- linux + X11
+            local pipe = io.popen("xclip -silent -in -selection clipboard", "w")
+            pipe:write(text)
+            pipe:close()
 
+        elseif command_exists("pbcopy") then
+            -- MacOS
+            local pipe = io.popen("pbcopy", "w")
+            pipe:write(text)
+            pipe:close()
+
+        else
+            mp.msg.error("no supported clipboard command found")
+        end
     else
         mp.msg.error("unknown platform " .. platform)
     end
@@ -44,16 +54,15 @@ end
 
 local function copyTime()
     local time_pos = mp.get_property_number("time-pos")
-    local time_in_seconds = time_pos
-    local time_seg = time_pos % 60
-    time_pos = time_pos - time_seg
-    local time_hours = math.floor(time_pos / 3600)
-    time_pos = time_pos - (time_hours * 3600)
-    local time_minutes = time_pos/60
-    time_seg, time_ms = string.format("%.03f", time_seg):match"([^.]*).(.*)"
-    time = string.format("%02d:%02d:%02d.%s", time_hours, time_minutes, time_seg, time_ms)
+    local minutes, remainder = divmod(time_pos, 60)
+    local hours, minutes = divmod(minutes, 60)
+    local seconds = math.floor(remainder)
+    local milliseconds = math.floor((remainder - seconds) * 1000)
+    local time = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+
     mp.osd_message(string.format("Copied to Clipboard: %s", time))
     set_clipboard(time)
 end
 
 mp.add_key_binding("Ctrl+c", "copyTime", copyTime)
+
